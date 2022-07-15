a581415dd2d27b563e5625a43bd2644728bc3fc4
Sourav Banerjee <rjsourav88@gmail.com>
2022-07-15 18:46:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getELementById("root"));
 
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
